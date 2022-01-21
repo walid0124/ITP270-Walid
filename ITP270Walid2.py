6bc788a5baa30ff210869b653c6862349a589436
Walid Ali <walidbd26@gmail.com>
2022-01-20 20:52:04 -0500

    Adding the file ITP270Walid2.py

---
+++ ITP270Walid2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print("Welcome to the ITP270 SPRING 2022 course!")
+
+user_choice = input("Do you want to proceed with the program?[Y/n]")
+
+if user_choice == "Y":
+	user_name = input("Enter your user name: ")
+	user_course = input("Enter the name of the course: ")
+	print("Thanks " + user_name + " for taking the course " + user_course + ".")
+else:
+	print("Have a good day!")
